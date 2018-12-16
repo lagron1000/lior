e949c29a6216ce040e577d4b272d9b7e834952df
unknown <lagron1000@gmail.com>
2018-12-16 19:35:08 +0200

    beter ng start

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const app = express();
 
 
 app.use(express.static('node_modules'));
 app.use(express.static(path.join(__dirname, 'dist/lior')));
-app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/lior/index.html'));
